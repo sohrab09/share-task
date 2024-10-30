f452f49eda4dc78fad195bd2fd0df6256d72268d
sohrab09 <sohrab.cse9@gmail.com>
2024-10-30 17:49:25 +0600

    Add a loading spinner.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import MainLayout from '../../layouts/MainLayout'
 import useProductStore from '../../state/productStore';
+import { Watch } from 'react-loader-spinner';
 
 const Home = () => {
 
@@ -10,8 +11,19 @@ const Home = () => {
         fetchProducts();
     }, [fetchProducts]);
 
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>Error: {error}</div>;
+    if (isLoading) return <div className="flex justify-center items-center h-screen">
+        <Watch
+            visible={true}
+            height="160"
+            width="160"
+            radius="48"
+            color="#4fa94d"
+            ariaLabel="watch-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+        />
+    </div>;
+    if (error) return <div className="flex justify-center items-center h-screen">Error: {error}</div>;
 
     return (
         <div className='mx-auto mt-20'>
