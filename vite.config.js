f514a8073b971fdda35d5b0b39e1ea2d65aad048
sohrab09 <sohrab.cse9@gmail.com>
2024-10-31 23:53:01 +0600

    Vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vercel()]
-});
+  plugins: [react()],
+})
